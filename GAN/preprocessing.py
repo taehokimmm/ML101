d2033c6ed2aec93b99999bc8e1f094209e0d2ce9
Taeho Kim <thkim0560@kaist.ac.kr>
2022-01-05 00:13:05 +0900

    data pipeline constructed

--- GAN/preprocessing.py
+++ GAN/preprocessing.py
@@ -1,7 +1,37 @@
+import os
 import torch
-
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 import torch
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms, datasets, utils
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Hyperparameters
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-print("Hello, world!")
+BATCHSIZE = 128
+
+# Construct Data Pipeline
+data_dir = os.path.join(ROOT_DIR, 'dataset')
+transform = transforms.Compose([transforms.ToTensor()])
+
+train_dataset = datasets.ImageFolder(root=data_dir, transform=transform)
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size = BATCHSIZE, num_workers = 2)
+
+test_dataset = datasets.ImageFolder(root=data_dir, transform=transform)
+test_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size = BATCHSIZE, num_workers = 2)
+
+def imshow(img): 
+    img = img.numpy()
+    plt.imshow(np.transpose(img, (1, 2, 0)))
+    plt.show()
+
+def show(): 
+    for batch_idx, (inputs, targets) in enumerate(train_dataloader):
+        imshow(inputs[0])
+
+
+
+if __name__ == '__main__':
+    show()
